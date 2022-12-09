282cacccd49930ca146e20060fb945ead85e8fba
phamhongphuc1403 <phamhongphuc1403@gmail.com>
2022-12-09 14:41:30 +0700

    update code

--- PBL2/passenger.cpp
+++ PBL2/passenger.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 Passenger::Passenger(string fullName, string phoneNumber, int bookedSeats, string destination, string carID, int departureTime, string departureDate, long long totalPrice)
-	:fullName(fullName), phoneNumber(phoneNumber), bookedSeats(bookedSeats), destination(destination), 
+	:fullName(fullName), phoneNumber(phoneNumber), bookedSeats(bookedSeats), destination(destination),
 	carID(carID), departureTime(departureTime), departureDate(departureDate), totalPrice(totalPrice) {
 	this->next = NULL;
 };
@@ -19,8 +19,8 @@ Passenger::Passenger(const Passenger& customer) {
 	this->next = customer.next;
 }
 ostream& operator << (ostream& out, const Passenger& customer) {
-	out << "Ten: " << customer.fullName << ", so dien thoai: " << customer.phoneNumber << ", so ghe da dat: " << customer.bookedSeats
-	<< ", diem den: " << customer.destination << ", bien so xe: " << customer.carID << ", ngay xuat phat: " << customer.departureDate 
-	<< ", gio: " << customer.departureTime << ", gia tien: " << customer.totalPrice << endl;
+	out << setiosflags(ios::left) << setw(15) << customer.fullName << setiosflags(ios::left) << setw(15) << customer.phoneNumber << setiosflags(ios::left) << setw(10) << customer.bookedSeats
+		<< setiosflags(ios::left) << setw(15) << customer.destination << setiosflags(ios::left) << setw(10) << customer.carID << setiosflags(ios::left) << setw(15) << customer.departureDate
+		<< setiosflags(ios::left) << setw(5) << customer.departureTime << setiosflags(ios::left) << setw(13) << customer.totalPrice << endl;
 	return out;
 }
